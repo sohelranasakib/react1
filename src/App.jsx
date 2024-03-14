4bfc08b57cb195b0489ed1d3f5135f318fd20ab3
sohelranasakib <sohelrany802@gmail.com>
2024-03-14 21:53:23 +0600

    demo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Banner from "./component/banner/Banner"
+import Menu from "./component/menu/Menu"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Menu/>
+    <Banner/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/menu/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./menu.css"
+
+const Menu = () => {
+  return (
+    <div className='main'> 
+        <ul>
+            <li><a href="#">home</a></li>
+            <li><a href="#">home</a></li>
+            <li><a href="#">home</a></li>
+            <li><a href="#">home</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Menu
